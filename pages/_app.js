15d7ec57113fea799c39113572ea0c1a452f5243
mehak <mehak1921@yahoo.com>
2021-06-06 21:07:16 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
-
+import '../styles/globals.scss'
+import Link from "next/link";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <nav className="header">
+        <div>
+          <Link href="/">
+            <a>
+              Kap's Kitchen
+  </a>
+          </Link>
+        </div>
+      </nav>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>)
 }
 
 export default MyApp
